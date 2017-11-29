79579090edcff9f7e9042ac628ec4eac96d64273
sida <siningdream@gmail.com>
2017-11-29 18:25:26 +0800

    Add docs and comments and clean code format

--- movie.py
+++ movie.py
@@ -1,12 +1,34 @@
 import webbrowser
 
 class Movie():
-    """docstring for Movie."""
+    """Describe Movie objects
+
+    Instance variables:
+    title -- the title of a movie
+    storyline -- the description of the movie storyline
+    poster_image_url --  the url of poster image
+    trailer_youtube_url -- the url to play the movie trailer
+
+    Public methods:
+    show_trailer -- open browser to play trailer for a movie
+    """
+
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+        """Form a movie object
+
+        Positional arguments:
+
+        1. title -- the title of a movie
+        2. storyline -- the description of the movie storyline
+        3. poster_image_url --  the url of poster image
+        4. trailer_youtube_url -- the url to play the movie trailer
+        """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
+        """Open browser to play trailer for a movie
+        """
         webbrowser.open(self.trailer_youtube_url)
--- play.py
+++ play.py
@@ -1,16 +1,23 @@
-import movie
-import fresh_tomatoes
+import movie # The library that contain Movie class
+import fresh_tomatoes # The library to render movies data to a html page
 
 movie1 = movie.Movie('Beauty and the Beast',
-                     'The story and characters you know and love come to spectacular life in the live-action adaptation of Disneys animated classic Beauty and the Beast, a cinematic event celebrating one of the most beloved tales ever told.',
+                     'The story and characters you know and love come to \
+                     spectacular life in the live-action adaptation of Disneys \
+                     animated classic Beauty and the Beast, a cinematic event \
+                     celebrating one of the most beloved tales ever told.',
                      'https://i.ytimg.com/vi_webp/g-DkY-drN9Q/movieposter.webp',
                      'https://www.youtube.com/watch?v=g-DkY-drN9Q')
 movie2 = movie.Movie('Cinderella',
-                     'The story of Cinderella follows the fortunes of young Ella (Lily James) whose merchant father remarries following the death of her mother.',
+                     'The story of Cinderella follows the fortunes of young \
+                     Ella (Lily James) whose merchant father remarries \
+                     following the death of her mother.',
                      'https://i.ytimg.com/vi_webp/UIYBoxiXsQw/movieposter.webp',
                      'https://www.youtube.com/watch?v=UIYBoxiXsQw')
 movie3 = movie.Movie('Snow White And The Seven Dwarfs',
-                     'Forever enchanting and inspiring, Snow White And The Seven Dwarfs embodies The Walt Disney Signature Collections legacy of animation.',
+                     'Forever enchanting and inspiring, Snow White And The \
+                     Seven Dwarfs embodies The Walt Disney Signature \
+                     Collections legacy of animation.',
                      'https://i.ytimg.com/vi_webp/vnkf6gZ5GDY/movieposter.webp',
                      'https://www.youtube.com/watch?v=vnkf6gZ5GDY')
 movie_list = [movie1, movie2, movie3]
